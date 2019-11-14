b9f18531a95c1a46ffe6253990c01c0dec5353cc
vishalikannan96 <53820281+vishalikannan96@users.noreply.github.com>
2019-11-14 17:28:00 +0530

    Add files via upload

---
+++ src/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='calculate_sin',
+    version='1.0.0',
+    url='',
+    license='',
+    data_files=[('.', ["__main__.py"])],
+    entry_points={'setuptools.installation': ['eggsecutable = src.main.main:main'], },
+    author='vishali.kannan',
+    author_email='',
+    description=''
+)
